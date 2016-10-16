1d56d88df1d09db83c0ca1f557161265ff4e48ed
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-10-16 11:54:24 -0700

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <div class ="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h1>My First Angular 2 App</h1>
+      </div>
+    </div>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
