a2c481a3cc990e6e49fbe85ec793830102892ffd
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-10-16 19:24:57 -0700

    add up to albums list display in lesson

---
+++ app/albums-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "albums";
+  template: `
+  <h3>Here are my favorite albums!</h3>
+  <div *ngFor="let currentAlbum of favoriteAlbums">
+    <p>{{currentAlbum}}</p>
+  </div>
+  `
+})
+
+export class AlbumsListComponent {
+  favoriteAlbums: string[] = ["The White Album", "Poison Cup", "2"];
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AlbumsListComponent} from "./albums-list.component";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AlbumsListComponent,
+    AppComponent
+  ],
   bootstrap: [AppComponent]
 })
 
